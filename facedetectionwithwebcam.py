39e133072932f240a90921cb6a49ed2ae28fbfd1
Lucas Phillips <53153598+luchophelps13@users.noreply.github.com>
2021-04-02 17:37:54 -0500

    Update facedetectionwithwebcam.py

--- facedetectionwithwebcam.py
+++ facedetectionwithwebcam.py
@@ -1,11 +1,6 @@
-
 import cv2
 
-# multiple cascades: https://github.com/Itseez/opencv/tree/master/data/haarcascades
-
-#https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-#https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
 cap = cv2.VideoCapture(0)
